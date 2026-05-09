ac084e3d15c5d3712cea419682a40f8380a00817
crajopanth <chandanmr@gmail.com>
2026-05-10 00:37:47 +1200

    Updated app.js to start with InitializeApp as the starting line

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ===== 1. Firebase init =====
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyBwyKPe1Cvqf9swwGJXD9SewTgvYz3l3Tc",
   authDomain: "pokerplanning-rajopanth.firebaseapp.com",
   databaseURL: "https://pokerplanning-rajopanth-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -8,7 +8,7 @@ const firebaseConfig = {
   messagingSenderId: "614873381520",
   appId: "1:614873381520:web:f498d924354cc1ecf27615"
 };
-
+*/
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
